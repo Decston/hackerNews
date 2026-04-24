d7242f73685e6106a5b8ccbc4e3fca270b3006df
Alisson Patricio Gomes de Freitas <alissonpatriciodefreitas@gmail.com>
2026-04-24 15:43:11 -0300

    feat: Atualizando a consulta para buscar o termo digitado na api hacker news.

--- src/App.js
+++ src/App.js
@@ -23,24 +23,32 @@ class App extends Component {
 
     //Faz o bind da função que vai atualizar o estado da variavel result, depois de trazer da api.
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    //Faz o bind da função que será reutilizavel para fazer busca na api do Hacker News.
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     //Faz o bind da função que altera o termo de busca, depois de alterado pelo usuário.
     this.onSearchChange = this.onSearchChange.bind(this);
+    //Faz o bind da função que obtem resultados da API Hacker News ao buscar no componente Search.
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     //Faz o bind da função que recebe o id e retira o item da lista de acordo com o id passado.
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  fetchSearchTopStories(searchTerm) {
+    //Executa uma chamada a api hacker news, buscando no endereço passado as noticias de acordo
+    //com o termo passado como parametro no searchTerm. Recebe a resposta em json e o resultado da
+    //requesição é setado na variavel result através do método setSearchTopStories.
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   //Esse função é executada logo após o componente ser montado.
   componentDidMount() {
     //Pega o valor default de searchTerm após o objeto ser montado.
     const { searchTerm } = this.state;
-  
-    //Executa uma chamada a api hacker news, buscando no endereço passado as noticias de acordo
-    //com o termo default da variavel searchTerm. Recebe a resposta em json e o resultado da
-    //requesição é setado na variavel result através do método setSearchTopStories.
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+    //Função que irá buscar noticias da api com o searchTerm default do estado do componente.
+    this.fetchSearchTopStories(searchTerm);
   }
 
   //Atualiza o estado da variavel result, sendo utilizada na primeira requisição feita a api.
@@ -53,9 +61,21 @@ class App extends Component {
 
   //Atualiza o termo que irá ser buscado a cada alteração do usuario no input Search.
   onSearchChange(event) {
-    //Pega o evento advindo do input de Search, depois de cada atualização e seta o valor
+    //Pega o evento advindo do input de Search, através do SyntheticEvent,
+    //depois de cada atualização e seta o valor
     //escrito no input para a variavel searchTerm no estado do componente.  
     this.setState({ searchTerm: event.target.value });
+  }
+
+  //Função que buscará dados da api de acordo com o valor já salvo na variavel searchTerm 
+  //do estado do componente e sendo executada somente após clicar no botão de pesquisa.
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    //Função que irá buscar notícias na api com o valor do searchTerm alterado no input Search. 
+    this.fetchSearchTopStories(searchTerm);
+    //Método para evitar que o browser recarregue o conteúdo, comportamento natural do navegador
+    //ao utilizar uma função de callback do submit.
+    event.preventDefault();
   }
 
   //Função que recebe o id de uma noticia da lista, passado pelo botão dismiss do componente
@@ -90,12 +110,12 @@ class App extends Component {
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >Search</ Search>
         </div>
         { result &&
           <Table
             list={result.hits}
-            pattern={searchTerm}
             onDismiss={this.onDismiss}
           />
         }
@@ -104,14 +124,21 @@ class App extends Component {
   }
 }
 
-const Search = ({ value, onChange, children }) =>
-  <form>
-    {children}
+const Search = ({ 
+  value, 
+  onChange, 
+  onSubmit, 
+  children 
+}) =>
+  <form onSubmit={onSubmit}>
     <input 
       type='text'
       value={value}
       onChange={onChange} 
     />
+    <button type='submit'>
+      {children}
+    </button>
   </form>
 
 const largeColumn = {
@@ -128,7 +155,7 @@ const smallColumn = {
 
 const Table = ({ list, pattern, onDismiss }) => 
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span style={ largeColumn }>
           <a href={item.url}>{item.title}</a>

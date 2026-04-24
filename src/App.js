4661a609f7bdd7e3553a7e41b59190edd0d5134f
Alisson Patricio Gomes de Freitas <alissonpatriciodefreitas@gmail.com>
2026-04-24 14:28:56 -0300

    feat: Criado o métod para buscar dados da api Hacker News e atualizar a lista.

--- src/App.js
+++ src/App.js
@@ -17,45 +17,72 @@ class App extends Component {
     super(props);
 
     this.state = {
-      result: null,
-      searchTerm: DEFAULT_QUERY,
+      result: null, //Armazena o resultado da lista de noticias hackers que serão renderizadas.
+      searchTerm: DEFAULT_QUERY, //Armazena o termo a ser buscado através do input 
     };
 
+    //Faz o bind da função que vai atualizar o estado da variavel result, depois de trazer da api.
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    //Faz o bind da função que altera o termo de busca, depois de alterado pelo usuário.
     this.onSearchChange = this.onSearchChange.bind(this);
+    //Faz o bind da função que recebe o id e retira o item da lista de acordo com o id passado.
     this.onDismiss = this.onDismiss.bind(this);
   }
 
+  //Esse função é executada logo após o componente ser montado.
   componentDidMount() {
+    //Pega o valor default de searchTerm após o objeto ser montado.
     const { searchTerm } = this.state;
   
+    //Executa uma chamada a api hacker news, buscando no endereço passado as noticias de acordo
+    //com o termo default da variavel searchTerm. Recebe a resposta em json e o resultado da
+    //requesição é setado na variavel result através do método setSearchTopStories.
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
   }
 
+  //Atualiza o estado da variavel result, sendo utilizada na primeira requisição feita a api.
+  //Acontecendo após o objeto ser montado.
   setSearchTopStories(result) {
+    //Pega a lista de noticias advinda da api hacker news passado como parametro
+    //e seta na variavel result do state.
     this.setState({ result });
   }
 
+  //Atualiza o termo que irá ser buscado a cada alteração do usuario no input Search.
   onSearchChange(event) {
+    //Pega o evento advindo do input de Search, depois de cada atualização e seta o valor
+    //escrito no input para a variavel searchTerm no estado do componente.  
     this.setState({ searchTerm: event.target.value });
   }
 
+  //Função que recebe o id de uma noticia da lista, passado pelo botão dismiss do componente
+  //Table, onde será feita uma filtragem dessa noticia a partir do seu id e atualizada a
+  //variavel result do state através de um método spread, recebendo a lista já existente e
+  //a nova lista filtrada.
   onDismiss = (id) => {
-    const updatedHits = this.state.result.hits.filter(
-      item => item.objectID !== id
-    );
+    //Cria uma função isNotId para armazenar o valor booleano, testando se o id do item é igual 
+    //ao id que foi passado como parametro. 
+    const isNotId = item => item.objectID !== id;
+    //Cria a const updatedHits para armazenar a nova lista que filtra o item que teve seu id 
+    //repassado como parametro. (Se o id do item for diferente do id repassado 
+    //retorna true) Com isso ao testar a função isNotId, só vai tirar da nova lista quando 
+    //o id do item for igual ao id passado como parametro. 
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    //Atualiza o valor da variavel result utilizando o operador Spread, pois o objeto result
+    //advindo da api do hacker news tem mais atributos do que a lista filtrada. Assim atualizamos
+    //apenas o atributo hits do result.
     this.setState({ 
-      result: { ...this.state.result, hits: updatedHits } 
-    })
+      result: { ...this.state.result, hits: updatedHits }
+    });
   }
 
+  //Método que renderiza o jsx do componente.
   render() {
+    //Faz a desestruturação do estado nas variaveis searchTerm e result.
     const { searchTerm, result } = this.state;
-    
-    if (!result) return null; // guarda enquanto carrega
 
     return (
       <div className="page">
@@ -65,11 +92,13 @@ class App extends Component {
             onChange={this.onSearchChange}
           >Search</ Search>
         </div>
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result &&
+          <Table
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        }
       </div>
     );
   }
@@ -109,7 +138,7 @@ const Table = ({ list, pattern, onDismiss }) =>
         <span style={ smallColumn }>{item.points}</span>
         <span style={ smallColumn }>
           <Button
-            onClick={() => onDismiss(item.objectId)}
+            onClick={() => onDismiss(item.objectID)}
             className="button-inline"
           >Dismiss</Button>
         </span>
